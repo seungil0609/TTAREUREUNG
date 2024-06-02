32b6864ac9c64ee4c90361b41559aa8d4a492608
rlatmddlf <142652289+seungil0609@users.noreply.github.com>
2024-06-02 15:39:12 +0900

    Update share.js

--- js/share.js
+++ js/share.js
@@ -3,7 +3,7 @@ const url = 'https://seungil0609.github.io/keirincontest/';
 function setShare(){
   var resultImg = document.querySelector('#resultImg');
   var resultAlt = resultImg.firstElementChild.alt;
-  const shareTitle = '나만의 경륜 승식은 ';
+  const shareTitle = '나만의 경륜 승식은';
   const shareDes = infoList[resultAlt].name;
   const shareImage = url + 'img/image-' + resultAlt + '.png';
   const shareURL = url + 'page/result-' + resultAlt + '.html';
